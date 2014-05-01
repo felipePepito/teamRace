ed646de33fce0526de5acc7c8dc4ab54bab7ece8
felipePepito <ph.strobl@gmx.de>
2014-05-01 09:13:59 +0200

    css asset included

--- symfony/app/Resources/views/TeamRace.html.php
+++ symfony/app/Resources/views/TeamRace.html.php
@@ -6,18 +6,23 @@
 		<title><?php $view['slots']->output('title', 'teamRace') ?></title>
 		
         <?php $view['slots']->output('stylesheets') ?>
+        
+        <link href="<?php echo $view['assets']->getUrl('assets/css/mainLayout.css') ?>" rel="stylesheet" type="text/css" />
+        
 		
-		<link rel="shortcut icon" href="<?php echo $view['assets']->getUrl('favicon.ico') ?>" />
-    
 	</head>
 
 	<body>
 
+		<div id="navigation">
 		<?php $view['slots']->output('navigation'); ?>
-		
+		</div>
+		<div id="content">
 		<?php $view['slots']->output('content'); ?>
+		</div>
 		
 		<?php $view['slots']->output('javascripts') ?>
+		
 
 	</body>
 </html>
--- symfony/src/TeamRace/ExternalBundle/Resources/views/layout.html.php
+++ symfony/src/TeamRace/ExternalBundle/Resources/views/layout.html.php
@@ -2,9 +2,9 @@
 
 <?php $view['slots']->start('navigation'); ?>
 
-<a id="navigationHome" href="<?php echo $view['router']->generate('TeamRaceExternalIndex', array(), true); ?>">teamRace</a><span id="navigationSpacer"></span>
-<a id="navigationItem" href="<?php echo $view['router']->generate('TeamRaceExternalAbout', array(), true); ?>">about</a><span id="navigationSpacer"></span>
-<a id="navigationItem" href="<?php echo $view['router']->generate('TeamRaceExternalLogin', array(), true); ?>">login</a><span id="navigationSpacer"></span>
-<a id="navigationItem" href="<?php echo $view['router']->generate('TeamRaceExternalRegister', array(), true); ?>">register</a><span id="navigationSpacer"></span>
+<a id="navigationHome" href="<?php echo $view['router']->generate('TeamRaceExternalIndex', array(), true); ?>">teamRace</a><span class="navigationSpacer"></span>
+<a class="navigationItem" href="<?php echo $view['router']->generate('TeamRaceExternalAbout', array(), true); ?>">about</a><span class="navigationSpacer"></span>
+<a class="navigationItem" href="<?php echo $view['router']->generate('TeamRaceExternalLogin', array(), true); ?>">login</a><span class="navigationSpacer"></span>
+<a class="navigationItem" href="<?php echo $view['router']->generate('TeamRaceExternalRegister', array(), true); ?>">register</a><span class="navigationSpacer"></span>
 
 <?php $view['slots']->stop(); ?>
